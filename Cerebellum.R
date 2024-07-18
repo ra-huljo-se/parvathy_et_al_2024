80196cf4220711dada380dc382f6284e50947bed
rahul <160262280+ra-huljo-se@users.noreply.github.com>
2024-07-18 18:33:47 +0530

    Add files via upload

---
+++ Cerebellum.R
@@ -0,0 +1,11 @@
+#Find gene expression in different cell types of cerebellum at different dev stages
+#Data obtained from https://doi.org/10.1016/j.cub.2018.07.062
+#Analysis by Rahul Jose
+#
+
+library(tidyverse)
+library(gdata)
+library(Seurat)
+library(clusterProfiler)
+
+#Adding the codes soon
